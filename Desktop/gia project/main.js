ff470f9104c880051d6ea653a58734eccbcc5f1d
Ying Liu <av8orly@gmail.com>
2026-02-26 18:29:38 +0800

    add project files

---
+++ Desktop/gia project/main.js
@@ -0,0 +1,22 @@
+// Vanta Birds background
+VANTA.BIRDS({
+  el: "#vanta-bg",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0x050510,
+  color1: 0x00f5ff,
+  color2: 0xbf00ff,
+  colorMode: "lerp",
+  birdSize: 1.2,
+  wingSpan: 28.00,
+  speedLimit: 4.00,
+  separation: 60.00,
+  alignment: 50.00,
+  cohesion: 50.00,
+  quantity: 3.00
+});
